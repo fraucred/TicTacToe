98597562bf39f3861a11bdb3f5428e79de789b8b
David MA <david.ma@lacombedulionvert.fr>
2022-02-02 11:25:25 +0100

    Initial commit : add pom.xml & FrameworkTest to test JUnit

---
+++ src/test/java/FrameworkTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FrameworkTest {
+
+    @Test
+    public void should_successfully_execute_common_unit_test() {
+        assertTrue(false);
+    }
+}
